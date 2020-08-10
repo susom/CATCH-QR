c370eaa619217e93db94652eecf18d2c7ed47730
Jordan Schultz <jmschultz@ucdavis.edu>
2020-08-10 15:48:29 -0700

    switched order, necessary for routing correctly for static files

--- server/index.js
+++ server/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(require('./routes'));
 app.use(express.static(path.join(__dirname, "..", "build")))
-
+app.use(require('./routes'));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
