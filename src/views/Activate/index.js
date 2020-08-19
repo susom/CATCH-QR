92e8960cd8bcd6f782b3ccb2eccf966685513aad
Jordan Schultz <jmschultz@ucdavis.edu>
2020-08-19 14:57:17 -0700

    Created testing application for temporary validation

--- src/views/Activate/index.js
+++ src/views/Activate/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import PinField from 'react-pin-field';
 import './Activate.css';
 import { makeStyles } from '@material-ui/core/styles';
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, responsiveFontSizes } from "@material-ui/core/styles";
 import Header from '../../components/Header';
 import { Typography, Box, Snackbar, IconButton } from '@material-ui/core';
 import StyledButton from '../../components/StyledButton';
@@ -35,15 +35,12 @@ class Activate extends React.Component{
         
         this.state = {
             kitId: c,
-            error: '',
             loading: true,
+            success:false,
+            open: false
         }
     }
-    componentDidUpdate() {
-        if(this.ref.current){
-            this.ref.current[0].value = 'V'
-        }
-    }
+    
 
     componentDidMount(){
         if(this.state.kitId){ //navigated here with a code
@@ -59,119 +56,21 @@ class Activate extends React.Component{
             return (this.ref.current.map((arg)=>arg.value)).join('')
     }
 
-    //Perform redirect here, replace url
-    redirect = (payload) => {
-        const url = payload.redirect_url
-        var {kitId} = this.state
-        if(!kitId)
-            kitId = this.getCodeFromRef()
-        var res = url.replace("[kit_id]", kitId);
-        if(res)
-            window.location = res
-        else
-            this.setState({error: 'No Redirect entry found, please contact your admin'})
-    }
-
-    //Skeleton error handling
-    handleError = (err) => {
-        this.setState({error: `Kit ID ${this.state.kitId} is invalid or expired , please try again`, kitId:'', loading:false})
-    }
 
     //Close popup error message
     handleClose = () => {
-        this.setState({error:''})
+        this.setState({open:false})
     }
 
-    //Callback for activate button press
+    
     onComplete = () => {
-        const check = this.state.kitId ? this.state.kitId : this.getCodeFromRef() //code will only be set if triggered from onComplete in pin Component
-        if(this.validate(check)){
-            let data = {
-                'kitId': check
-            };
-    
-            axios.post(`/api/activate`, data)
-                .then(res=>this.redirect(JSON.parse(res?.data?.project)))
-                .catch(err=>this.handleError(err))
-        } else {
-            this.setState({error:'Kit ID appears invalid, please double check and try again', kitId:'', loading: false, })
-        }
-        
+        this.setState({
+            open:true, 
+            successMessage:'Success! Your kit has been activated.', 
+            success:true,
+            loading:false
+        })
     }
-
-    // Function that validates QR code before sending request to server
-    validate = (code) => {
-        if(!code)
-            return false
-        
-        var validChars  = "234689ACDEFHJKMNPRTVWXY";
-        code            = code.toUpperCase().trim().split("").reverse(); //prep code for algo UPPERCASe, TRIM , REVERSE
-        var verifyDigit = code.shift(); 
-        var checkSum    = 0;
-        
-        // make sure code portion consists of valid chars
-        var checkvalid  = code.filter(char => validChars.indexOf(char) == -1);
-        if(checkvalid.length){
-            console.log("Invalid Character(s) in Code");
-            return false;
-        }
-        // apply algo to code reversed "right to left"
-        for (var i in code) {
-            var char = code[i];
-            var prep_ord_digit = char.charCodeAt(0) - 48;
-            var weight;
-            if (i % 2 == 0) {
-                weight = (2 * prep_ord_digit) - parseInt(prep_ord_digit / 5) * 9;
-            } else {
-                weight = prep_ord_digit;
-            }
-            checkSum += weight;
-        }
-        
-        checkSum        = Math.abs(checkSum) + 10;
-        var checkDigit  = (10 - (checkSum % 10)) % 10;
-        return checkDigit == verifyDigit;
-    }
-    
-    // Which validation function is this ? Andy provided
-    // validateCodeFormatMod = (code) => {
-    //     if(!code)
-    //         return false;
-    //     // Flip an array so key is value - assumes array is unique
-    //     function array_flip( trans ) {
-    //         var key, tmp_ar = {};
-    //         for ( key in trans ) {
-    //             if ( trans.hasOwnProperty( key ) ) {
-    //                 tmp_ar[trans[key]] = key;
-    //             }
-    //         }
-    //         return tmp_ar;
-    //     }
-    //     // Prepare Valid Chars
-    //     // validChars is a string such as "234689ACDEFHJKMNPRTVWXY"
-    //     var validChars = VCG.validChars.split("");
-    //     // Flip Chars
-    //     var validKeys = array_flip(validChars);
-    //     // Prepare Code
-    //     var arrChars = code.trim().split("");
-    //     // Remove checkDigit
-    //     var lastDigit = arrChars.pop();
-    //     // Calc CheckDigit using mod
-    //     var idxSum = 0;
-    //     for (i in arrChars) {
-    //         char = arrChars[i];
-    //         var k = parseInt(validKeys[char]);
-    //         if (isNaN(k)) {
-    //             console.log('invalid character in code: ' + char + ' -- ignoring it for checksum');
-    //         } else {
-    //             idxSum = idxSum + k;
-    //         }
-    //     }
-    //     var mod = idxSum % validChars.length;
-    //     // Convert mod index to actual character (e.g. 6 becomes A)
-    //     var checkDigit = validChars[mod];
-    //     return checkDigit == lastDigit;
-    // }
 
 
     //Loading screen placeholder
@@ -199,6 +98,21 @@ class Activate extends React.Component{
         )
     }
 
+    renderSuccess = () => {
+        return (
+            <Grid container direction='column'>
+                <Grid item >
+                    <Typography variant='h4' style={{marginTop:'1em'}} >Congratulations!</Typography>
+                </Grid>
+                <Grid item style={{marginTop:'3em'}} >
+                    <Typography>Your kit has been activated. Please continue using 
+                        the provided paper instructions included in the test
+                        package starting at <strong>step 2</strong>.</Typography>
+                </Grid>
+            </Grid>
+        )
+    }
+
     render(){
         //VXCPTP9
         return (
@@ -212,34 +126,39 @@ class Activate extends React.Component{
                         alignItems="center"
                         justify="center"
                     >
+                        
                         <Grid className = 'vera-icon' container alignItems="center" justify='center'>
                             <Grid item >
                                 
                             </Grid>
                         </Grid>
-                        <Grid item className = 'qr-container' container direction='row' alignItems='center' justify='center' xs={7}>
-                            <Grid item className='qr-container-int' container direction='column' alignItems='center' justify='center' xs={10}>
-                                <Grid item >
+                        <Grid item className = 'qr-container' container direction='row' alignItems='center' justify='center'>
+                            <Grid item className='qr-container-int' container alignItems='center' justify='center' xs={10}>
+                                <Grid item xs={12}>
                                     <Box textAlign='center'>
-                                        <h2 >Enter your Kit ID</h2>
-                                        {/* Put popup here with hover. */}
+                                    {this.state.success ? this.renderSuccess() : <h2 >Enter your Kit ID</h2>}
+                                        
                                     </Box>
                                 </Grid>
-                                <Grid item >
+                                <Grid item xs={12}>
                                     <div className="container-a">
-                                        <PinField
-                                            className={"field-a"}
-                                            format={k => k.toUpperCase()}
-                                            ref={this.ref}
-                                            length={7}
-                                        />
+                                        {! this.state.success && 
+                                            <PinField
+                                                className={"field-a"}
+                                                format={k => k.toUpperCase()}
+                                                ref={this.ref}
+                                                length={7}
+                                            />
+                                        }
                                     </div>
                                 </Grid>
-                                <StyledButton 
-                                    text='Activate' 
-                                    onClick={()=>this.onComplete()} 
-                                    style={{borderRadius:50, marginBottom:'10px', color:'white', width:'250px', height:'50px', fontSize:'20px'}} 
-                                />
+                                {!this.state.success &&
+                                    <StyledButton 
+                                        text='Activate' 
+                                        onClick={()=>this.onComplete()} 
+                                        style={{borderRadius:50, marginBottom:'10px', color:'white', width:'250px', height:'50px', fontSize:'20px'}} 
+                                    />
+                                }
                             </Grid>
                         </Grid>
                     </Grid>
@@ -250,10 +169,10 @@ class Activate extends React.Component{
                         vertical: 'bottom',
                         horizontal: 'left',
                     }}
-                    open={this.state.error ? true : false}
+                    open={this.state.open ? true : false}
                     autoHideDuration={6000}
                     onClose={this.handleClose}
-                    message={this.state.error}
+                    message={this.state.successMessage}
                     action={
                         <IconButton size="small" aria-label="close" color="inherit" onClick={this.handleClose}>
                             <CloseIcon fontSize="small" />
