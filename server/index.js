73e56458d44614e7616675eba4bb41071f01beca
Jordan Schultz <jmschultz@ucdavis.edu>
2020-12-22 13:29:43 -0800

    invalidating routes, only pushing to catch url

--- server/index.js
+++ server/index.js
@@ -3,10 +3,25 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, "..", "build")))
+// app.use(express.static(path.join(__dirname, "..", "build"))) commented out for redirecting only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('./routes'));
+// app.use(require('./routes')); commented out for redirect only
+
+app.get('/activate')
+app.use(function timeLog(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET')
+  next();
+});
+
+app.get('*', (req, res)=> {
+  res.redirect(301, `https://catchstudy.stanford.edu${req.originalUrl}`);
+});
+
+
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
